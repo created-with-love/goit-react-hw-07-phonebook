055aba75e76b4c095f6daf068d258ccefa92bf75
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-01-18 21:09:07 +0200

    base for project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import React from 'react';
+import Form from './components/Form';
+import Section from './components/Section';
+import Contacts from './components/Contacts';
+import Filter from './components/Filter';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Section title="Phonebook">
+        <Form />
+      </Section>
+
+      <Section title="Contacts">
+        <Filter />
+        <Contacts />
+      </Section>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { FaTrash, FaAddressCard } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
+import { motion } from 'framer-motion';
+import s from './Contacts.module.css';
+import actions from '../../redux/contacts/contacts-action';
+import { filteredContacts } from '../../redux/contacts/contacts-selectors';
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.7,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+};
+
+const Contacts = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(filteredContacts);
+
+  const listClasses = classNames({
+    [s.list]: true,
+    'list-border': contacts.length,
+  });
+
+  return (
+    <div className={listClasses}>
+      <ul className={s.list__ul}>
+        {contacts.map(person => (
+          <motion.li
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            key={person.id}
+            className={s.search__contact}
+          >
+            <IconContext.Provider
+              value={{
+                color: 'black',
+                size: '1.6em',
+                className: 'react-icons',
+              }}
+            >
+              <FaAddressCard
+                onClick={() => dispatch(actions.deleteContact(person.id))}
+              />
+            </IconContext.Provider>
+            <p className={s.search__text}>
+              {person.name} : {person.number}
+            </p>
+            <div className={s.trash}>
+              <IconContext.Provider
+                value={{
+                  color: 'red',
+                  size: '1.1em',
+                  className: 'react-icons',
+                }}
+              >
+                <FaTrash
+                  onClick={() => dispatch(actions.deleteContact(person.id))}
+                />
+              </IconContext.Provider>
+            </div>
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Contacts;
